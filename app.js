a4b229e641622cef75a5dc1b939be35a9d95498b
sury1999 <suryansusengupta@gmail.com>
2020-06-13 19:43:48 +0530

    Third Commit, Working

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const secondHand = document.querySelector('.secondHand')
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds/60)*360);
+setInterval(setClock,1000)
 
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setClock()
+{
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation (secondHand, secondsRatio);
+  setRotation (minuteHand, minutesRatio);
+  setRotation (hourHand, hoursRatio);
 }
-setInterval(setDate, 1000);
 
+function setRotation(element,rotationRatio)
+{
+  element.style.setProperty('--rotation', rotationRatio * 360);
+}
 
-
-
+setClock();
 
 })
