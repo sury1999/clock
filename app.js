71582b14f03c00d06d7c178788f5f6cc6fc52974
sury1999 <suryansusengupta@gmail.com>
2020-06-20 01:33:10 +0530

    Second clock needs work

--- app.js
+++ app.js
@@ -24,4 +24,22 @@ function setRotation(element,rotationRatio)
 
 setClock();
 
+const hourHand2 = document.querySelector('[data-hour2-hand2]');
+const minuteHand2 = document.querySelector('[data-minute2-hand2]');
+const secondHand2 = document.querySelector('[data-second2-hand2]');
+
+function setClock2()
+{
+  const currentDate2 = new Date();
+  const secondsRatio2 = currentDate2.getSeconds() / 60;
+  const minutesRatio2 = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio2 = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation2 (hourHand2, hoursRatio2);
+}
+
+function setRotation2(element,rotationRatio2)
+{
+  element.style.setProperty('--rot', rotationRatio2 * 360);
+}
+
 })
