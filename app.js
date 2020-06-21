58e0e1dc0d4c4c723c44f69504443a2b3747e6f0
sury1999 <suryansusengupta@gmail.com>
2020-06-22 00:20:18 +0530

    Hour hand Fixed, Rest not so much

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 setInterval(setClock,1000)
+setInterval(setClock2,1000)
 
 const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
@@ -32,14 +33,20 @@ function setClock2()
 {
   const currentDate2 = new Date();
   const secondsRatio2 = currentDate2.getSeconds() / 60;
-  const minutesRatio2 = (secondsRatio + currentDate.getMinutes()) / 60;
-  const hoursRatio2 = (minutesRatio + currentDate.getHours()) / 12;
+  const minutesRatio2 = (secondsRatio2 + currentDate2.getMinutes()) / 60;
+  const hoursRatio2 = (minutesRatio2 + currentDate2.getHours()) / 12;
   setRotation2 (hourHand2, hoursRatio2);
+  setRotation2 (minuteHand2, minutesRatio2);
+  setRotation2 (secondHand2, secondsRatio2);
 }
 
 function setRotation2(element,rotationRatio2)
 {
   element.style.setProperty('--rot', rotationRatio2 * 360);
 }
+
+
+
+setClock2();
 
 })
